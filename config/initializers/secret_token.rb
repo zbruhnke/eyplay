06406b48c9a4e91347a941240264544d7e433b0b
Zach Bruhnke <z@zbruhnke.com>
2012-04-03 11:21:28 -0700

    pushing more

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eyplay::Application.config.secret_token = '7469f32865a33632e5c62553dd8e47307aea2f57bd12e7aa3e703aab2e42db5af3c0823ebffb7f452581a3dc771e8bd7266bbfe3581445aa9c3ec4ca86cb4807'
